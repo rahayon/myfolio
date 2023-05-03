9d84613b8f43548ef3c8dfe7f1cae5ec19501680
Rahidul Islam <rahiseli@outlook.com>
2023-05-03 14:06:09 +0600

    .gitignore is now working

--- core/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import MyProject, MyService
-# Register your models here.
-admin.site.register(MyProject)
-admin.site.register(MyService)
--- core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from django.urls import path
-from .views import HomeView
-app_name = 'core'
-
-urlpatterns = [
-    path('', HomeView.as_view(), name='home')
-]
